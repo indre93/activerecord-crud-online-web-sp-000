efcd3c808ed81487bc80c522fbf84aa94954847d
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 21:56:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,13 +23,13 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(movie={})
+def can_be_created_in_a_block(args = nil)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
 
-  movie = Movie.create do |m|
-    m.title = "#{m}"
+  Movie.create do |m|
+    create(m)
   end
 end
 
