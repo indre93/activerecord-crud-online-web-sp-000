9ec0b19e43d1e931c88b72fe2607614d2fdd2dd0
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 22:03:53 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,8 +28,8 @@ def can_be_created_in_a_block(args = {})
   # title == "Home Alone"
   # release_date == 1990
 
-  args = Movie.create do |m|
-    self.title = "#{m}"
+   Movie.create do |m|
+    args = "#{m}"
   end
 end
 
